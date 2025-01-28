3dbe239fa69e2a53d9147980531cefc27e49f91f
Shahrza <smuqbilli@gmail.com>
2025-01-28 18:42:43 +0400

    refactor API service to use headers for API key instead of request parameters

--- src/services/API.ts
+++ src/services/API.ts
@@ -5,14 +5,9 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const API: AxiosInstance = axios.create({
   baseURL: BASE_URL,
-});
-
-API.interceptors.request.use((config) => {
-  config.params = {
-    ...config.params,
-    apiKey: API_KEY,
-  };
-  return config;
+  headers: {
+    "X-Api-Key": API_KEY,
+  },
 });
 
 export const fetcher = <T>(url: string): Promise<T> =>
