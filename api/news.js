5fd475aa3daf5c3c9e846114b8907a0a3fffd424
Shahrza <smuqbilli@gmail.com>
2025-02-22 12:03:31 +0400

    fix

--- api/news.js
+++ api/news.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  const url = `https://newsapi.org/v2/${
+  const url = `${process.env.VITE_BASE_URL}${
     req.query.endpoint
   }?${new URLSearchParams(req.query).toString()}`;
   const response = await fetch(url, {
-    headers: { Authorization: `Bearer ${process.env.NEWS_API_KEY}` },
+    headers: { Authorization: `Bearer ${process.env.VITE_API_KEY}` },
   });
 
   const data = await response.json();
